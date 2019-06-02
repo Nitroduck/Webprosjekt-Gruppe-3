0d16ff48f5cf62654067007d2c36fd11369ef4b5
Nora <nbergseth@live.no>
2019-06-02 17:22:23 +0200

    oppdatering6

---
+++ Løsning/js/login.js
@@ -0,0 +1,12 @@
+/*let emailInput = document.getElementById("emailInp");
+let passwordInput = document.getElementById("passwordInput");
+let finalLogin = document.getElementById("finalLogin");
+let status = document.getElementById("staus");
+
+function login() {
+    if (emailInput === "kari@nordmann.no" && passwordInput === "kari123") {
+        finalLogin.createElement("board.html");
+    } else {
+        status.innerText = "Wrong email or password";
+    }
+}*/
