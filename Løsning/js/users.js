eb64354f06315d21afa0c007cb1c446667fcf615
Nitroduck <42619350+Nitroduck@users.noreply.github.com>
2019-06-02 20:00:27 +0200

    Added objects
    placeholder objects for use in js

---
+++ Løsning/js/users.js
@@ -0,0 +1,21 @@
+var user = [
+    {
+        name: "placeholder1",
+        epost: "place@holder.no1",
+        username: "placeholder1",
+        password: "123456"        
+    },
+    {
+        name: "placeholder2",
+        epost: "place@holder.no2",
+        username: "placeholder2",
+        password: "123456"        
+    },
+    {
+        name: "placeholder3",
+        epost: "place@holder.no3",
+        username: "placeholder3",
+        password: "123456"        
+    },
+];
+
