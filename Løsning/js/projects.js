6985e2984f25ad0cac15889d040052da3e13f6b5
Nitroduck <42619350+Nitroduck@users.noreply.github.com>
2019-06-03 17:34:13 +0200

    created objects
    - objects for users, projects, tasks etc

---
+++ Løsning/js/projects.js
@@ -0,0 +1,12 @@
+let projects = [
+    {
+        id: 1,
+        name: "Project1",
+        description: "Cool project"        
+    },
+    {
+        id: 2,
+        name: "Project2",
+        description: "Lame project"
+    }
+];
---
+++ Løsning/js/users.js
@@ -0,0 +1,23 @@
+var users = [
+    {
+        id: 1,
+        name: "Peter Petersen",
+        email: "peter@petersen.com",
+        username: "peterp",
+        password: "peter123"
+    },
+    {
+        id: 2,
+        name: "Gunnar Gunnarsen",
+        email: "gunnar@gunnarsen.com",
+        username: "gunnarg",
+        password: "gunnar123",        
+    },
+    {
+        id: 3,
+        name: "Nilsette Nilsen",
+        email: "nilsette@nilsen.com",
+        username: "nilsetten",
+        password: "nilsette123",
+    }
+];
