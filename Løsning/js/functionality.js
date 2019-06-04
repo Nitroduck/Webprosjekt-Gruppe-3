7111cfbaf6bc974637d944c2913156d25baf8402
Nitroduck <42619350+Nitroduck@users.noreply.github.com>
2019-06-05 00:18:15 +0200

    Update syntax structure
    - removed file redundancy

---
+++ Løsning/js/functionality.js
@@ -0,0 +1,23 @@
+function set(property, value){ //SETTER
+        document.getElementById(property).innerHTML = value;
+}
+
+/* Work in progress
+
+function verifyLogin() {
+    
+    for(i = 0; 1 < users.length; i++) {
+        if (document.getElementById("uname") == users[i].username) 
+            && (document.getElementById("pword") == users[i].password){
+                
+            }
+    }
+}
+
+*/
+
+
+
+/* function get(property){ GETTER
+        return document.getElementById(property).innerHTML;        
+}; */
