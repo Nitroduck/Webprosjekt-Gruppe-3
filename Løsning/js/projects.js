d418364810b3cd905bcc393d08d1f0f63be1e90f
remmar18 <ps3-martinremme@hotmail.no>
2019-06-07 01:04:01 +0200

    Update - Final update
    Big update, cleaing up code, functions for board, project. Button redesign and bug check.

--- Løsning/js/projects.js
+++ Løsning/js/projects.js
@@ -1,18 +1,9 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
-     });
-}); 
+    });
+});
 
-//Drag and drop function//
-
-if (itemID == null){
-        var itemID = 0;
-    };
-
-
-$('#createProject').on("click", function() {
-itemID++;
-var $box = $("#board").append('<button class="btn btn-primary" id="linkButton" onClick="reply_click(this.id) style="float:left" <p id="prosjekt_add+'+itemID+'"></p></button>');
-
+$('#addItem').on("click", function () {
+    var $box = $("#board").append('<div id="projectList"><a class="btn btn-primary"  id="linkButton" href="board.html" data-role="button" ></a><p id="prosjekt_navn">New project</p></div>');
 });
--- Løsning/js/user.js
+++ Løsning/js/user.js
@@ -1,21 +1,20 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
 
-        $(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-            });
-        });
+// Set userdata//
+set("user-name", users[0].name);
+set("my-mail", users[0].email);
+set("my-dob", users[0].dob);
 
-        // Set userdata//
-        set("user-name", users[0].name);
-        set("my-mail", users[0].email);
-        set("my-dob", users[0].dob);
-
-        // Enter new uservalues//
-        function myFunction() {
-            var fName = document.getElementById("fullName").value;
-            document.getElementById("user-name").innerHTML = fName;
-            var myMail = document.getElementById("mail").value;
-            document.getElementById("my-mail").innerHTML = myMail;
-            var myDob = document.getElementById("dob").value;
-            document.getElementById("my-dob").innerHTML = myDob;
-        }
+// Enter new uservalues//
+function myFunction() {
+    var fName = document.getElementById("fullName").value;
+    document.getElementById("user-name").innerHTML = fName;
+    var myMail = document.getElementById("mail").value;
+    document.getElementById("my-mail").innerHTML = myMail;
+    var myDob = document.getElementById("dob").value;
+    document.getElementById("my-dob").innerHTML = myDob;
+}
